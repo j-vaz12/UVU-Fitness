e6433d3263c86f067ab9ef430b568e9bbdeb4b1e
j-vaz12 <jasonvasquezx@gmail.com>
2025-08-02 15:37:10 -0700

    mockup landing page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,21 @@
 
-const Navbar = () =>{
-    return(
-        <nav>
-            <h1> UVU Fitness </h1>
-        </nav>
-    );
-}
+import React from "react";
+import "./Navbar.css";
 
-export default Navbar; 
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar__logo">UVU FITNESS</div>
+      <ul className="navbar__links">
+        <li><a href="#">About Me</a></li>
+        <li><a href="#">Testimonials</a></li>
+        <li><a href="#">Pricing</a></li>
+        <li><a href="#">Location</a></li>
+        <li><a href="#">Blog</a></li>
+      </ul>
+      <a href="#" className="navbar__cta">Schedule Now</a>
+    </nav>
+  );
+};
+
+export default Navbar;
